ce2b60832e69111a2a8ede63043bd032e1c57a52
chance <chance+sft@utc24.io>
2023-05-09 22:17:13 -0500

    feat: initial commit

---
+++ packages/protocol/hardhat.config.ts
@@ -0,0 +1,44 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-spdx-license-identifier"
+import "hardhat-tracer";
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.8.17",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 2000,
+                details: {
+                    yul: true,
+                    yulDetails: {
+                        stackAllocation: true,
+                        optimizerSteps: "u"
+                    }
+                }
+            }
+        },
+    },
+    paths: {
+        sources: "./src",
+    },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS ? true : false,
+        currency: 'USD',
+        gasPrice: 40,
+        onlyCalledMethods: false,
+        showMethodSig: true,
+    },
+    typechain: {
+        outDir: 'src/types',
+        target: 'ethers-v5',
+    },
+    spdxLicenseIdentifier: {
+        overwrite: true,
+        runOnCompile: true
+    }
+};
+
+export default config;
